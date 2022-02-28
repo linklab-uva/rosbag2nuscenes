699eb559b911155635d5d3e017e76c878ac4e99a
John Chrosniak <jlc9wr@virginia.edu>
2022-02-28 18:24:46 -0500

    added sample.json implementation

--- scene_writer.py
+++ scene_writer.py
@@ -15,21 +15,16 @@ def write_scene(rosbag_file, track_name):
         print("Track name not recognized, must be LVMS or IMS")
         exit(1)
     with Reader(rosbag_file) as reader:
-        # Create scene.json
-        first_scene, last_scene = '', ''
-        log_token = ''
-        with open('scene.json', 'w', encoding='utf-8') as outfile:
+        # Create log.json
+        log_token = token_hex(16)
+        with open('log.json', 'w', encoding='utf-8') as outfile:
             data = dict()
-            data['token'] = token_hex(16)
-            log_token = token_hex(16)
-            data['log_token'] = log_token
-            connection = [x for x in reader.connections.values() if x.topic == '/novatel_top/dyntf_odom'][0]
-            data['nbr_samples'] = connection.count
-            first_scene, last_scene = token_hex(16), token_hex(16)
-            data['first_sample_token'] = first_scene
-            data['last_sample_token'] = last_scene
-            data['name'] = 'scene-0001'  # TODO: increase global variable to create name
-            data['description'] = track_name
+            data['token'] = log_token
+            data['logfile'] = ''
+            data['vehicle'] = 'Cavalier'
+            date_captured = datetime.fromtimestamp(reader.start_time * 1e-9).strftime('%Y-%m-%d')
+            data['date_captured'] = date_captured
+            data['location'] = track_name
             json.dump([data], outfile, ensure_ascii=False, indent=4)
         # Create map.json
         # Case where map file exists
@@ -61,21 +56,48 @@ def write_scene(rosbag_file, track_name):
                 lvms_data['category'] = 'LVMS'
                 lvms_data['filename'] = '' # TODO: double check we aren't including map mask
                 json.dump([ims_data, lvms_data], outfile, ensure_ascii=False, indent=4)
-        # Create log.json
-        with open('log.json', 'w', encoding='utf-8') as outfile:
-            data = dict()
-            data['token'] = log_token
-            data['logfile'] = ''
-            data['vehicle'] = 'Cavalier'
-            date_captured = datetime.fromtimestamp(reader.start_time * 1e-9).strftime('%Y-%m-%d')
-            data['date_captured'] = date_captured
-            data['location'] = track_name
-            json.dump([data], outfile, ensure_ascii=False, indent=4)
         connections = [x for x in reader.connections.values() if (x.topic == '/novatel_top/dyntf_odom' or x.topic == '/luminar_points')]
-        for connection, timestamp, rawdata in reader.messages(connections=connections):
-            if connection.topic == '/novatel_top/dyntf_odom':
-                msg = deserialize_cdr(rawdata, connection.msgtype)
-                print(msg.header)
+        # Create sample.json
+        # TODO: Case where sample.json already exists
+        first_scene, last_scene = '', ''
+        prev_scene, next_scene = '', ''
+        scene_token = token_hex(16)
+        samples = []
+        with open('sample.json', 'w', encoding='utf-8') as outfile:
+            for connection, timestamp, rawdata in reader.messages(connections=connections):
+                data = dict()
+                data['timestamp'] = timestamp
+                data['scene_token'] = scene_token
+                data['prev'] = prev_scene
+                prev_scene = next_scene
+                next_scene = token_hex(16)
+                data['next'] = next_scene  
+                if first_scene == '':
+                    prev_scene = token_hex(16)
+                    first_scene = prev_scene
+                samples.append(data)
+            samples[-1]['next'] = ''
+            json.dump(samples, outfile, ensure_ascii=False, indent=4)
+        last_scene = prev_scene
+                
+
+                
+                # if connection.topic == '/novatel_top/dyntf_odom':
+                #     msg = deserialize_cdr(rawdata, connection.msgtype)
+                #     print(msg.header)
+        # Create scene.json
+        with open('scene.json', 'w', encoding='utf-8') as outfile:
+            data = dict()
+            data['token'] = scene_token
+            data['log_token'] = log_token
+            connection = [x for x in reader.connections.values() if x.topic == '/novatel_top/dyntf_odom'][0]
+            data['nbr_samples'] = connection.count
+            first_scene, last_scene = token_hex(16), token_hex(16)
+            data['first_sample_token'] = first_scene
+            data['last_sample_token'] = last_scene
+            data['name'] = 'scene-0001'  # TODO: increase global variable to create name
+            data['description'] = track_name
+            json.dump([data], outfile, ensure_ascii=False, indent=4)
 
 if __name__=="__main__":
     if len(sys.argv) != 3:
